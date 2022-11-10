8d746927a141b3e04e7cd70223033624dac8b392
João <108736974+JoaoVitorRSilva@users.noreply.github.com>
2022-11-10 15:13:48 -0300

    Criando arquivo ContaJogo.py

---
+++ atividade 4/ContaJogo.py
@@ -0,0 +1,24 @@
+def cria_conta(idd, nick, cash, level):
+    conta = {'idd': idd, 'nick': nick, 'cash': cash, 'level': level}
+    return conta
+
+def pay(conta, valor):
+    conta['cash'] += valor
+
+def reembolso(conta, valor):
+    if (conta['cash'] < valor):
+        print('Valor Invalido')
+    else:
+        conta['cash'] -= valor
+        return True
+    
+def uplevel(conta, valor):
+    conta['level'] += valor
+
+def extrato(conta):
+    print('Conta\n Id:',conta['idd'], '\n  Nick:',conta['nick'], '\n   Cash:',conta['cash'], '\n    Level:',conta['level'])
+
+contaJao = cria_conta('9391-0', 'Jao', 2000.0, 124.0)
+reembolso(contaJao, 500.0)
+uplevel(contaJao, 5)
+extrato(contaJao)
