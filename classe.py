8fb3891baaa2e73bf2809d4a6c3c47f807e5fc7e
João <108736974+JoaoVitorRSilva@users.noreply.github.com>
2022-11-10 15:32:44 -0300

    Criando arquivo classe.py

---
+++ classe.py
@@ -0,0 +1,31 @@
+class Real:
+    def __init__(self, nome, sobrenome, idade):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+
+class Conta:
+    def __init__(self, idd, nick, cash, level):
+        self.idd = idd
+        self.nick = nick
+        self.cash = cash
+        self.level = level
+
+    def pay(self, valor):
+        self.cash += valor
+
+    def reembolso(self, valor):
+        if (self.cash < valor):
+            print('Cash atual é', self.cash, ', o valor da tentativa de reembolso foi,', valor)
+            print('Valor Invalido')
+        else:
+            print('Cash atual é,', self.cash,', o valor do reembolso foi,', valor)
+            print('Valor Reembolsado')
+            self.cash -= valor
+            return True
+        
+    def uplevel(self, valor):
+        self.level += valor
+
+    def extrato(self):
+        print('Conta\n Id:',self.idd, '\n  Nick:', self.nick, '\n   Cash:', self.cash, '\n    Level:', self.level)
